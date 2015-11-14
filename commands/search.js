fee343c0ebc5b8bc868e7d5467594112561b558d
Jeff <woz5999@gmail.com>
2015-11-14 11:36:07 -0600

    Initial commit

---
+++ commands/search.js
@@ -0,0 +1,58 @@
+var slackAPI = require('../slackAPI');
+var Global = require('../global');
+var querystring = require('querystring');
+
+//search command
+var search = {
+    search: function(query, res) {
+        var path = '/api/search.all';
+        var args = querystring.stringify({
+            token: Global.token,
+            query: query,
+            sort: 'timestamp'
+        });
+
+        slackAPI.sendRequest(args, path, search.parseResults, res);
+    },
+
+    parseResults: function(body, res) {
+        var results = [];
+
+        res.status('200');
+
+        if(body.messages.matches) {
+            var matches = body.messages.matches;
+
+            for(var key in matches) {
+                if(matches[key] &&
+                    matches[key].type == 'message') {
+                    var item = {
+                        title: matches[key].username,
+                        text: matches[key].text
+                    };
+
+                    if(matches[key].channel) {
+                        item.channel = matches[key].channel.name;
+                    }
+
+                    results.push(item);
+                }
+            }
+        }//end check matches if
+
+        if(results && results.length > 0) {
+            res.json({
+                text: 'Search results',
+                attachments: results
+            });
+        } else {
+            res.json({
+                text: 'No results'
+            });
+        }
+
+        return;
+    }
+};
+
+module.exports = search.search;
