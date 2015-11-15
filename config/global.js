25abdec763b03fc9e05a5b6627bfbb17ddcf38d8
Jeff <woz5999@gmail.com>
2015-11-15 15:00:36 -0600

    Remove redundant authentication variable

--- config/global.js
+++ config/global.js
@@ -3,11 +3,6 @@ var global = {};
 switch(process.env.NODE_ENV) {
     case('dev'):
         global = {
-            appTokens: {
-                'T4ZdouaGsqCxIEUC6ddj50Lv':
-                    true
-            },
-
             authTokens: {
                 'T4ZdouaGsqCxIEUC6ddj50Lv':
                     'xoxp-14577477094-14577477126-14571825524-a14226976c'
@@ -17,7 +12,6 @@ switch(process.env.NODE_ENV) {
 
     default:
         global = {
-            appTokens: JSON.parse(process.env.appTokens),
             authTokens: JSON.parse(process.env.authTokens)
         };
         break;
